fbf7f4bd9fd7a6ba64d5edae0b6c3184ec742463
alvasilvao <asilvao@ug.uchile.cl>
2023-03-09 21:02:20 +0100

    'msg

--- up.py
+++ up.py
@@ -2,4 +2,6 @@ import subprocess
 
 subprocess.run(["cd","~"])
 subprocess.run(["git","add","."])
-subprocess.run(["git","commit","-m","'msg"])
+subprocess.run(["git","commit","-m","'msg'"])
+subprocess.run(["git","branch","-M","main"])
+subprocess.run(["git","push","-u","origin","main"])
