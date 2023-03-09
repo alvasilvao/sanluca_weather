9c1b03c6716546a37684751efb3834e280ac2290
alvasilvao <asilvao@ug.uchile.cl>
2023-03-09 21:07:37 +0100

    abcms

--- up.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-subprocess.run(["cd","~"])
-subprocess.run(["git","add","."])
-subprocess.run(["git","commit","-m","'msg'"])
-subprocess.run(["git","branch","-M","main"])
-subprocess.run(["git","push","-u","origin","main"])
